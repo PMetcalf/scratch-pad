823d32b1cda3b445dac8175ae96b73246905f40a
PMetcalf <paulmetcalf@hotmail.com>
2021-02-19 15:54:23 +0000

    Work in progress.

--- Web Services/python_api_integration.py
+++ Web Services/python_api_integration.py
@@ -6,3 +6,12 @@ Best practices for integrating an API into a workflow.
 
 # Module imports
 import requests
+
+resp = requests.get('https://todolist.example.com')
+
+# Handle erroneous responses
+if resp.status_code != 200:
+    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+
+for todo_item in resp.json():
+    print('{} {}'.format(todo_item['id'], todo_item['summary']))
