ca702f099fa083a8c6c7366f2a7b1dcc69c0ee87
PMetcalf <paulmetcalf@hotmail.com>
2021-02-19 16:02:56 +0000

    Work in progress.

--- Web Services/python_api_integration.py
+++ Web Services/python_api_integration.py
@@ -10,14 +10,30 @@ import requests
 # Constants
 base_url = 'https://todolist.example.com/tasks/'
 
-# --- Getting data ---
+# --- Getting Data (Get Task) ---
 
+# Get task
 resp = requests.get(base_url)
 
 # Handle erroneous responses
 if resp.status_code != 200:
     raise ApiError('GET /tasks/ {}'.format(resp.status_code))
 
+# Create task object from response
 for todo_item in resp.json():
     print('{} {}'.format(todo_item['id'], todo_item['summary']))
+
+# --- Posting Data (Get Task) ---
+
+# Define task
+task = {"summary": "Take out trash", "description": ""}
+
+# Post task
+resp = requests.post(base_url, json = task)
+
+# Handle erroneous response
+if resp.status_code != 201:
+    raise ApiError('POST /tasks/ {}'.format(resp.status_code))
+
+print('Created task, ID: {}'.format(resp.json()["id"]))
 
