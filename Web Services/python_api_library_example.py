5ceb2dc8dbf5f6206a185b202d3fe058933f5994
PMetcalf <paulmetcalf@hotmail.com>
2021-02-19 16:19:27 +0000

    Work in progress.

--- Web Services/python_api_library_example.py
+++ Web Services/python_api_library_example.py
@@ -46,3 +46,17 @@ print('Created task, ID: {}'.format(resp.json()["id"]))
 
 class todo:
 
+    def get_tasks():
+        pass
+    
+    def describe_task(task_id):
+        pass
+
+    def add_task(summary, description = ""):
+        pass
+
+    def task_done(task_id):
+        pass
+
+    def update_task(task_id, summary, description):
+        pass
