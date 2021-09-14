fef8fb0cbb3186421b1e7e49b30bd7e2338a6c14
PMetcalf <paulmetcalf@hotmail.com>
2021-09-14 09:27:05 +0100

    Wip.

--- keras/undercomplete_linear_autoencoder.py
+++ keras/undercomplete_linear_autoencoder.py
@@ -3,3 +3,10 @@ The following code builds a simple linear autoencoder to perform PCA on a 3D dat
 '''
 
 from tensorflow import keras
+
+encoder = keras.models.Sequential([keras.layers.Dense(2, input_shape =[3])])
+decoder = keras.models.Sequential([keras.layers.Dense(3, input_shape = [2])])
+
+autoencoder = keras.models.Sequential([encoder, decoder])
+
+autoencoder.compile(loss = 'mse', optimizer = keras.optimizers.SGD(lr = 0.1))
