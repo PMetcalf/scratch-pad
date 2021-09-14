d06059602f483364757f6cb3a3b5c0064454392f
PMetcalf <paulmetcalf@hotmail.com>
2021-09-14 09:52:20 +0100

    Wip.

--- keras/stacked_autoencoder.py
+++ keras/stacked_autoencoder.py
@@ -3,6 +3,7 @@ The following code builds a stacked autoencoder for Fashion MNIST (loaded and no
 '''
 
 from tensorflow import keras
+from tensorflow.keras import optimizers
 
 stacked_encoder = keras.models.Sequential([
     keras.layers.Flatten(input_shape = [28, 28]),
@@ -15,3 +16,11 @@ stacked_decoder = keras.models.Sequential([
     keras.layers.Dense(28 * 28, activation = 'sigmoid'),
     keras.layers.Reshape([28, 28])      # Reshape the dense layer output to match the encoder input
 ])
+
+stacked_autoencoder = keras.models.Sequential([stacked_encoder, stacked_decoder])
+
+optimizer = keras.optimizers.SGD(lr = 1.5)
+
+stacked_autoencoder.compile(loss = 'binary_crossentropy', optimizers = optimizer)
+
+history = stacked_autoencoder.fit(X_train, X_train, epochs = 10, validation_data = [X_valid, X_valid])
