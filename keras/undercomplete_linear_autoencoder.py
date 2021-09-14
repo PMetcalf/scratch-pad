96783c3cd1988c8df1c3b1e1c3070be40dc943b3
PMetcalf <paulmetcalf@hotmail.com>
2021-09-14 09:30:56 +0100

    Wip.

--- keras/undercomplete_linear_autoencoder.py
+++ keras/undercomplete_linear_autoencoder.py
@@ -7,6 +7,11 @@ from tensorflow import keras
 encoder = keras.models.Sequential([keras.layers.Dense(2, input_shape =[3])])
 decoder = keras.models.Sequential([keras.layers.Dense(3, input_shape = [2])])
 
+# Autoencoder is a Sequential model containing encoder followed by decoder
 autoencoder = keras.models.Sequential([encoder, decoder])
 
 autoencoder.compile(loss = 'mse', optimizer = keras.optimizers.SGD(lr = 0.1))
+
+history = autoencoder.fit(X_train, X_train, epochs = 20)
+
+codings = encoder.predict(X_train)
