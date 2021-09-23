c36e9217c1de59f8f3b0b146806767f01b8ec4d4
PMetcalf <paulmetcalf@hotmail.com>
2021-09-23 09:01:27 +0100

    Wip.

--- keras/stacked_autoencoder_tying_weights.py
+++ keras/stacked_autoencoder_tying_weights.py
@@ -2,3 +2,23 @@
 The following code ties weights between encoder and decoder in a stacked autoencoder
 '''
 
+# Define a custom layer
+class DenseTranspose(keras.layers.Layer):
+
+    def __init__(self, dense, activation = None, **kwargs):
+
+        self.dense = dense
+
+        self.activation = keras.activations.get(activation)
+
+        super().__init__(**kwargs)
+
+    def build(self, batch_input_shape):
+
+        self.biases = self.add_weight(name = "bias",
+                                        initializer = "zeros",
+                                        shape = [self.dense.input_shape[-1]])
+
+        super().build(batch_input_shape)
+
+    
