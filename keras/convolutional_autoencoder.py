dbf91b780a3885b40306bc03832f385b483cfac8
PMetcalf <paulmetcalf@hotmail.com>
2021-09-27 09:58:53 +0100

    Wip.

--- keras/convolutional_autoencoder.py
+++ keras/convolutional_autoencoder.py
@@ -2,3 +2,15 @@
 Convolutional Autoencoder for processing MNIST fashion dataset
 '''
 
+import keras
+
+# Build the encoder
+conv_encoder = keras.models.Sequential([
+    keras.layers.Reshape([28, 28, 1], input_shape = [28, 28]),
+    keras.layers.Conv2D(16, kernel_size = 3, padding = 'same', activation = 'selu'),
+    keras.layers.MaxPool2D(pool_size = 2),
+    ])
+
+# Build the decoder
+
+# Build the complete assembly
