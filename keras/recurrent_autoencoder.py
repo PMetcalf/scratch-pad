414c17be1b16d8f1093d45d4ca8f265f8bc53542
PMetcalf <paulmetcalf@hotmail.com>
2021-10-01 08:59:35 +0100

    Work in progress.

---
+++ keras/recurrent_autoencoder.py
@@ -0,0 +1,11 @@
+'''
+Recurrent Autoencoder 
+
+[Encoder] Sequence to Vector RNN -> [Decoder] Vector to Sequence RNN
+'''
+
+# Build the encoding layer
+
+# Build the decoding layer
+
+# Assemble the encoder
