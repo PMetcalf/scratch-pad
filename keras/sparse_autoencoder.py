4314f54b91421c11aa7f42573deabb376e0e3f95
PMetcalf <paulmetcalf@hotmail.com>
2021-10-20 11:27:39 +0100

    Wip.

--- keras/sparse_autoencoder.py
+++ keras/sparse_autoencoder.py
@@ -4,3 +4,10 @@ Sparse Autoencoder
 Adds some regularisation to the coding layer's inputs to improve feature extraction per neuron.
 '''
 
+import keras
+
+# Create the regularised encoder
+
+# Create the decoder
+
+# Build the autoencoder
