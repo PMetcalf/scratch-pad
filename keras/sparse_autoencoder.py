c74c538248da298cbbc9c115fde2d0765eaacc35
PMetcalf <paulmetcalf@hotmail.com>
2021-10-20 11:31:23 +0100

    Wip.

--- keras/sparse_autoencoder.py
+++ keras/sparse_autoencoder.py
@@ -7,6 +7,12 @@ Adds some regularisation to the coding layer's inputs to improve feature extract
 import keras
 
 # Create the regularised encoder
+sparse_l1_encoder = keras.models.Sequential([
+    keras.layers.Flatten(input_shape = [28, 28]),
+    keras.layers.Dense(100, activation = 'selu'),
+    keras.layers.Dense(300, activation = 'sigmoid'),    # This layer is regularised to improve feature extraction
+    keras.layers.ActivityRegularization(l1 = 1e-3)  # This regularisation improves feature extraction
+])
 
 # Create the decoder
 
