78ba85c006a6eaa01ec909599858590a7bc6b3d2
PMetcalf <paulmetcalf@hotmail.com>
2021-10-20 11:34:10 +0100

    Wip.

--- keras/sparse_autoencoder.py
+++ keras/sparse_autoencoder.py
@@ -11,9 +11,14 @@ sparse_l1_encoder = keras.models.Sequential([
     keras.layers.Flatten(input_shape = [28, 28]),
     keras.layers.Dense(100, activation = 'selu'),
     keras.layers.Dense(300, activation = 'sigmoid'),    # This layer is regularised to improve feature extraction
-    keras.layers.ActivityRegularization(l1 = 1e-3)  # This regularisation improves feature extraction
+    keras.layers.ActivityRegularization(l1 = 1e-3)      # This regularisation improves feature extraction
 ])
 
 # Create the decoder
+sparse_l1_decoder = keras.models.Sequential([
+    keras.layers.Dense(100, activation = 'selu', input_shape = [300]),  # Work with the regularised layer
+    keras.layers.Dense(28 * 28, activation = 'sigmoid'),
+    keras.layers.Reshape([28, 28])
+])
 
 # Build the autoencoder
