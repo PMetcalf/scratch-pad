7a9643e0b1443937cf56ddcb190d6aab13150af9
PMetcalf <paulmetcalf@hotmail.com>
2021-10-25 10:19:05 +0100

    Wip.

--- keras/sparse_autoencoder_kl_divergence.py
+++ keras/sparse_autoencoder_kl_divergence.py
@@ -12,3 +12,19 @@ import keras
 K = keras.backend
 
 kl_divergence = keras.losses.kullback_leibler_divergence
+
+# Create custom regulariser to apply KL divergence regularisation
+class KLDivergenceRegulariser(keras.regularizers.Regularizer):
+
+    def __init__(self, weight, target = 0.1):
+        
+        self.weight = weight
+        self.target = target
+
+    def __call__(self, inputs):
+
+        mean_activities = K.mean(inputs, axis = 0)
+
+        return self.weight * (
+            kl_divergence(self.target, mean_activities) + kl_divergence(1. - self.target, 1. - mean_activities)
+        )
