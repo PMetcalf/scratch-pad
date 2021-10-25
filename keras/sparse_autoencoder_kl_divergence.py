ae903323261b6a58f4da5360c6ec546c0909e79c
PMetcalf <paulmetcalf@hotmail.com>
2021-10-25 10:13:40 +0100

    Wip.

---
+++ keras/sparse_autoencoder_kl_divergence.py
@@ -0,0 +1,8 @@
+'''
+Sparse Autoencoder with KL (Kullback-Leibler) Divergence
+
+Adds some regularisation to the coding layer's inputs to improve feature extraction per neuron, 
+with tunable sparsity in the input layer, which seeks to balance the activation of neurons.
+
+Setup to work with the MNIST clothing images dataset.
+'''
