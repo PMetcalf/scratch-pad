fd2b2765dfcf9e6b68ae9680303a13c0dab74e41
PMetcalf <paulmetcalf@hotmail.com>
2021-10-27 08:43:37 +0100

    Work in progress.

---
+++ keras/variational_autoencoder.py
@@ -0,0 +1,10 @@
+'''
+Variational Autoencoder
+
+Uses a Gaussian distribution between encoding and decoding layers to add a layer of filtering to inputs
+and enable generation of simulated outputs.
+
+Works with MNIST fashion dataset.
+'''
+
+import keras
