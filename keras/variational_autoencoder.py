3c15e7cc7e5f89270d3a2f5fba50903ab1545900
PMetcalf <paulmetcalf@hotmail.com>
2021-10-27 08:48:14 +0100

    Wip.

--- keras/variational_autoencoder.py
+++ keras/variational_autoencoder.py
@@ -8,3 +8,16 @@ Works with MNIST fashion dataset.
 '''
 
 import keras
+import tensorflow as tf
+
+K = keras.backend
+
+# Custom layer is used to sample codings
+class Sampling(keras.layers.Layer):
+
+    def call(self, inputs):
+
+        mean, log_var = inputs
+
+        # Calculate and return latent loss
+        return K.random_normal(tf.shape(log_var)) * K.exp(log_var / 2) + mean
