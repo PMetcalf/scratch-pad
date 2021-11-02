2af21cc7ed9c46473977903639dc76a4b43b65a1
PMetcalf <paulmetcalf@hotmail.com>
2021-11-02 08:21:27 +0000

    Work in progress.

--- keras/variational_autoencoder.py
+++ keras/variational_autoencoder.py
@@ -21,3 +21,18 @@ class Sampling(keras.layers.Layer):
 
         # Calculate and return latent loss
         return K.random_normal(tf.shape(log_var)) * K.exp(log_var / 2) + mean
+
+# Create the encoder, using Functional API as model is not entirely Sequential
+codings_size = 10
+
+inputs = keras.layers.Input(shape = [28, 28])
+z = keras.layers.Flatten() (inputs)
+z = keras.layers.Dense(150, activation = 'selu') (z)
+z = keras.layers.Dense(100, activation = 'selu') (z)
+codings_mean = keras.layers.Dense(codings_size) (z)     # Mew ~ mean
+codings_log_var = keras.layers.Dense(codings_size) (z)  # Gamma ~ Log of std squared
+
+codings = Sampling() ([codings_mean, codings_log_var])
+
+# Assemble the encoder
+variational_encoder = keras.Model( inputs = [inputs], outputs = [codings_mean, codings_log_var, codings])
