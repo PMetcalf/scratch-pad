d1f83cda52152bf4636749958180c1a5d6bed87c
PMetcalf <paulmetcalf@hotmail.com>
2021-11-08 10:43:13 +0000

    Work in progress.

--- keras/simple_gan.py
+++ keras/simple_gan.py
@@ -11,14 +11,14 @@ codings_size = 30
 
 # Structure the generator
 generator = keras.models.Sequential([
-    keras.layers.Dense(100, activation = 'selu', input_shape = [codings_size]),
+    keras.layers.Dense(100, activation = 'selu', input_shape = [codings_size]),     # Input is shaped for noise
     keras.layers.Dense(150, activation = 'selu'),
     keras.layers.Dense(28 * 28, activation = 'sigmoid'),
     keras.layers.Reshape([28, 28])
 ])
 
 # Structure the discriminator
-discriminator = keras.models.Sequential([
+discriminator = keras.models.Sequential([           # Discriminator is a regular binary classifier
     keras.layers.Flatten(input_shape = [28, 28]),
     keras.layers.Dense(150, activation = 'selu'),
     keras.layers.Dense(100, activaiton = 'selu'),
