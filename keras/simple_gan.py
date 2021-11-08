b96b9f5487cb11c6730a8e3bfc246627462f3f5e
PMetcalf <paulmetcalf@hotmail.com>
2021-11-08 10:38:33 +0000

    Work in progress.

--- keras/simple_gan.py
+++ keras/simple_gan.py
@@ -6,3 +6,15 @@ Uses a generator and discriminator to develop hig-fidelity fake images of the MN
 '''
 
 import keras
+
+codings_size = 30
+
+# Structure the generator
+generator = keras.models.Sequential([
+    keras.layers.Dense(100, activation = 'selu', input_shape = [codings_size]),
+    keras.layers.Dense(150, activation = 'selu'),
+    keras.layers.Dense(28 * 28, activation = 'sigmoid'),
+    keras.layers.Reshape([28, 28])
+])
+
+# Structure the discriminator
